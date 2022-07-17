ed0d8af91752e36c4b34f079ad5402e216691cd7
75py <dev.75py@gmail.com>
2022-07-17 23:30:33 +0900

    rename package name

--- Sources/com.elgato.template.sdPlugin/app.js
+++ Sources/com.elgato.template.sdPlugin/app.js
@@ -16,14 +16,14 @@ $SD.on('connected', (jsonObj) => connected(jsonObj));
 
 function connected(jsn) {
     // Subscribe to the willAppear and other events
-    $SD.on('com.elgato.template.action.willAppear', (jsonObj) => action.onWillAppear(jsonObj));
-    $SD.on('com.elgato.template.action.keyUp', (jsonObj) => action.onKeyUp(jsonObj));
-    $SD.on('com.elgato.template.action.sendToPlugin', (jsonObj) => action.onSendToPlugin(jsonObj));
-    $SD.on('com.elgato.template.action.didReceiveSettings', (jsonObj) => action.onDidReceiveSettings(jsonObj));
-    $SD.on('com.elgato.template.action.propertyInspectorDidAppear', (jsonObj) => {
+    $SD.on('com.nagopy.streamdeck.tasktimer.action.willAppear', (jsonObj) => action.onWillAppear(jsonObj));
+    $SD.on('com.nagopy.streamdeck.tasktimer.action.keyUp', (jsonObj) => action.onKeyUp(jsonObj));
+    $SD.on('com.nagopy.streamdeck.tasktimer.action.sendToPlugin', (jsonObj) => action.onSendToPlugin(jsonObj));
+    $SD.on('com.nagopy.streamdeck.tasktimer.action.didReceiveSettings', (jsonObj) => action.onDidReceiveSettings(jsonObj));
+    $SD.on('com.nagopy.streamdeck.tasktimer.action.propertyInspectorDidAppear', (jsonObj) => {
         console.log('%c%s', 'color: white; background: black; font-size: 13px;', '[app.js]propertyInspectorDidAppear:');
     });
-    $SD.on('com.elgato.template.action.propertyInspectorDidDisappear', (jsonObj) => {
+    $SD.on('com.nagopy.streamdeck.tasktimer.action.propertyInspectorDidDisappear', (jsonObj) => {
         console.log('%c%s', 'color: white; background: red; font-size: 13px;', '[app.js]propertyInspectorDidDisappear:');
     });
 };
